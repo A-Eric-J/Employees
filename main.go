563a86a5436900fe2e354e98e7cf0c14b13afcf1
amiirrallii <amirali007z@yahoo.com>
2023-10-23 22:53:57 +0300

    whichCityIsNear, departedCityIndexing and printResult functions added.

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func minStartTime(employees []Employee, visited []bool) int {
 
 // sortIndexes / we want to know the order index of sending
 // employees based on start working time
-func sortIndexes(employees []Employee, indexes []int) {
+func sortIndexes(employees []Employee, orderIndexes []int) {
 	// this array is for knowing that we have sent the employee or not,
 	// and you see that, at first the initialized values are false
 	// it means that we have not sent the employees yet
@@ -28,8 +28,65 @@ func sortIndexes(employees []Employee, indexes []int) {
 
 	for i := 0; i < len(employees); i++ {
 		minIndex := minStartTime(employees, visited)
-		indexes[i] = minIndex
+		orderIndexes[i] = minIndex
 		visited[minIndex] = true
+	}
+}
+
+// whichCityIsNear / this function works based on the table of distances in README.md
+func whichCityIsNear(cityName string, departCityCapacity []int) string {
+	switch cityName {
+	case "Munich":
+		if departCityCapacity[0] != 0 {
+			departCityCapacity[0]--
+			return "a"
+		} else if departCityCapacity[1] != 0 {
+			departCityCapacity[1]--
+			return "b"
+		} else if departCityCapacity[2] != 0 {
+			departCityCapacity[2]--
+			return "c"
+		}
+	case "Hamburg":
+		if departCityCapacity[1] != 0 {
+			departCityCapacity[1]--
+			return "b"
+		} else if departCityCapacity[2] != 0 {
+			departCityCapacity[2]--
+			return "c"
+		} else if departCityCapacity[0] != 0 {
+			departCityCapacity[0]--
+			return "a"
+		}
+	case "Berlin":
+		if departCityCapacity[0] != 0 {
+			departCityCapacity[0]--
+			return "a"
+		} else if departCityCapacity[1] != 0 {
+			departCityCapacity[1]--
+			return "b"
+		} else if departCityCapacity[2] != 0 {
+			departCityCapacity[2]--
+			return "c"
+		}
+
+	}
+	return ""
+}
+
+// departedCityIndexing / this function works based on whichCityIsNear function and indexing departed cities
+func departedCityIndexing(orderIndexes []int, employees []Employee, departCityCapacity []int) {
+
+	for i := 0; i < len(employees); i++ {
+		departedIndexing := whichCityIsNear(employees[orderIndexes[i]].city, departCityCapacity)
+		employees[orderIndexes[i]].departCityName = departedIndexing
+	}
+
+}
+
+func printResult(employees []Employee) {
+	for i := 0; i < len(employees); i++ {
+		fmt.Println(i+1, ")", "Name: ", employees[i].name, " City: ", employees[i].city, " StartTime: ", employees[i].startTime, " DepartCity: ", employees[i].departCityName)
 	}
 }
 
@@ -59,6 +116,7 @@ func main() {
 
 	sortIndexes(employees, orderIndexes)
 
-	fmt.Println(employees, orderIndexes, departCityCapacity)
+	departedCityIndexing(orderIndexes, employees, departCityCapacity)
+	printResult(employees)
 
 }
